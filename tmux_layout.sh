6aa33be3fca1a85e9383b10149c0c711c17ac134
Chris Smith <dewcansam.dev@mail.com>
2024-05-27 19:28:13 -0400

    Removed >> the junk lines Added >> displayed the file name because I was losing track if the   correct version was running.

--- tmux_layout.sh
+++ tmux_layout.sh
@@ -4,35 +4,16 @@
 #   @author         Christopher L Smith (dewcansam)
 #   @copyright      2024-05-26 22:56:32
 #   @file           tmux_layout_v-?.sh
-#   @version        1.0
-#   @date           2024-05-26 23:47:51
+#   @version        1.1
+#   @date           2024-05-27 01:08:02
 #   @details        To setup an preconfigured layout session
+#   @note           How to execute
+#                       $ . run_tmux.sh 2>/dev/null
 #   @bug            =
-#   @todo = 2024-01-27 01:53:08 CLS +++ added item
-#   @todo = 2023-08-14 19:39:42 CLS *** edited item
-#   @todo = 2024-01-17 11:01:25 CLS --- deleted item
-#   @todo = 2023-08-14 03:23:06 CLS ///
+#   @todo = 2024-05-27 01:08:14 CLS 
 #
-
-#tmux new-session -d
-#tmux new-session -s s9
-#tmux attach -t s9
-#tmux kill-session -t s9
-#tmux ls
-#tmux kill-window -t :1
-#tmux new-window \; split-window -d
-#tmux new-session -d 'vi /etc/passwd' \; split-window -d \; attach
-
-#tmux new-session -d 'vi /etc/passwd' \; split-window -d \; attach
-
-#tmux new-session -d \; \
-#  split-window -v -c "#{pane_current_path}" \; \
-#  new-window -c "#{pane_current_path}" \; \
-#  split-window -h -c "#{pane_current_path}" \; \
-#  new-window -c "#{pane_current_path}" \; \
-#  new-window -c "#{pane_current_path}" \; \
-#  new-window -c "#{pane_current_path}" \;
-
+echo "run_tmux_v-1-1.sh"
 tmux new -d; tmux split-window -dv; tmux new-window -d; tmux split-window -t :2 -dh; tmux new-window -d; tmux new-window -d; tmux new-window -d; tmux attach;
+sleep 5s; tmux kill-session; printf "tmux exiting ...."
 
 #####~~~~~ EOF
